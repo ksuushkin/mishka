e1d42933cc92fd429c49d643eb655638ee4a8296
Kseniya <kseniya.sotskova@gmail.com>
2019-05-19 23:36:27 +0300

    Улучшает код

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,9 +1,9 @@
 const menuNode = document.querySelector('.main-nav');
-const buttonNode = document.querySelector('.main-nav__toggler');
+const burgerNode = document.querySelector('.main-nav__toggler');
 
 menuNode.classList.remove('main-nav--nojs');
 
-buttonNode.addEventListener('click', function () {
-        buttonNode.classList.toggle('main-nav__toggler--opened');
-        menuNode.classList.toggle('main-nav--opened')
-    });
+burgerNode.addEventListener('click', function () {
+  burgerNode.classList.toggle('main-nav__toggler--opened');
+  menuNode.classList.toggle('main-nav--opened')
+});
