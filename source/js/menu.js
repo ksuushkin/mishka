eefd4dad3f43f4543b04dec745ea91204579a950
Kseniya <kseniya.sotskova@gmail.com>
2019-05-19 15:44:56 +0300

    Добавляет автоматизацию сборки

--- source/js/menu.js
+++ source/js/menu.js
@@ -2,7 +2,6 @@ const menuNode = document.querySelector('.main-nav');
 const buttonNode = document.querySelector('.main-nav__toggler');
 
 menuNode.classList.remove('main-nav--nojs');
-// menuNode.classList.remove('main-nav--opened');
 
 buttonNode.addEventListener('click', function () {
         buttonNode.classList.toggle('main-nav__toggler--opened');
